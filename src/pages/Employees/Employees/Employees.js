8807d2ad3b42abf79e2b407ec05b505480d0e353
AskatAsh <abdulaskat@gmail.com>
2023-01-04 17:03:58 +0600

    changed font wight of navigation path and added padding bg-color in employees page

--- src/pages/Employees/Employees/Employees.js
+++ src/pages/Employees/Employees/Employees.js
@@ -4,62 +4,64 @@ import { Link } from "react-router-dom";
 const Employees = () => {
   return (
     <div>
-      <div className="mb-5 text-black font-semibold">
-        Home {">"} <span className="font-normal">Employees</span>
+      <div className="mb-5 text-black font-normal text-base">
+        Home {">"} <span className="font-light">Employees</span>
       </div>
-      <div className="flex justify-between mb-5">
-        <h2 className=" text-lg font-semibold text-gray-700">
-          List of Employees
-        </h2>
-        <Link to="/addnewemployee">
-          {" "}
-          <button className=" btn btn-sm btn-primary">+ Add New</button>
-        </Link>
-      </div>
-      <div className="overflow-x-auto w-full">
-        <table className="table w-full">
-          {/* <!-- head --> */}
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Job</th>
-              <th>Favorite Color</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {/* <!-- row 1 --> */}
-            <tr>
-              <td>
-                <div className="flex items-center space-x-3">
-                  <div className="avatar">
-                    <div className="mask mask-squircle w-12 h-12">
-                      <img
-                        src="/tailwind-css-component-profile-2@56w.png"
-                        alt="Avatar Tailwind CSS Component"
-                      />
+      <div className="bg-white p-5 rounded-md">
+        <div className="flex justify-between mb-5">
+          <h2 className=" text-lg font-semibold text-gray-700">
+            List of Employees
+          </h2>
+          <Link to="/addnewemployee">
+            {" "}
+            <button className=" btn btn-sm btn-primary">+ Add New</button>
+          </Link>
+        </div>
+        <div className="overflow-x-auto w-full">
+          <table className="table w-full">
+            {/* <!-- head --> */}
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Job</th>
+                <th>Favorite Color</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* <!-- row 1 --> */}
+              <tr>
+                <td>
+                  <div className="flex items-center space-x-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle w-12 h-12">
+                        <img
+                          src="/tailwind-css-component-profile-2@56w.png"
+                          alt="Avatar Tailwind CSS Component"
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="font-bold">Hart Hagerty</div>
+                      <div className="text-sm opacity-50">United States</div>
                     </div>
                   </div>
-                  <div>
-                    <div className="font-bold">Hart Hagerty</div>
-                    <div className="text-sm opacity-50">United States</div>
-                  </div>
-                </div>
-              </td>
-              <td>
-                Zemlak, Daniel and Leannon
-                <br />
-                <span className="badge badge-ghost badge-sm">
-                  Desktop Support Technician
-                </span>
-              </td>
-              <td>Purple</td>
-              <th>
-                <button className="btn btn-ghost btn-xs">details</button>
-              </th>
-            </tr>
-          </tbody>
-        </table>
+                </td>
+                <td>
+                  Zemlak, Daniel and Leannon
+                  <br />
+                  <span className="badge badge-ghost badge-sm">
+                    Desktop Support Technician
+                  </span>
+                </td>
+                <td>Purple</td>
+                <th>
+                  <button className="btn btn-ghost btn-xs">details</button>
+                </th>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
