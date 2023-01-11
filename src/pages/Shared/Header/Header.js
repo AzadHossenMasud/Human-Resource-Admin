43eff52e60805f6713024993b70356a5c5594a46
Azad Hossen <azadhossen6677@gmail.com>
2023-01-11 14:21:02 +0600

    header error solve

--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -15,22 +15,12 @@ const Header = () => {
   }
   return (
     <div>
-<<<<<<< HEAD
       <div className="navbar bg-sky-900 text-neutral-content">
         <Link className="btn btn-ghost normal-case text-xl" to="/admin">
           HR
         </Link>
         <button className="btn" onClick={handleLogout}>Logout</button>
 
-=======
-      <div className="navbar bg-sky-900 text-neutral-content flex justify-between">
-        <Link className="btn btn-ghost normal-case text-xl ml-3" to="/">
-          HR
-        </Link>
-        <Link className="btn btn-ghost normal-case text-xl mr-3" to="../Login">
-          Login
-        </Link>
->>>>>>> 3c23d05932948640ad9fcf75fc2287838d6ad7c5
       </div>
     </div>
   );
