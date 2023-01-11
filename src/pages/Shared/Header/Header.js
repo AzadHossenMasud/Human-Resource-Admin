ac8f74e3b8cbcc81c108c1495eaaa167b689b62f
AskatAsh <abdulaskat@gmail.com>
2023-01-11 12:43:59 +0600

    login link added in the header section

--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -4,9 +4,12 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <div>
-      <div className="navbar bg-sky-900 text-neutral-content">
-        <Link className="btn btn-ghost normal-case text-xl" to="/">
+      <div className="navbar bg-sky-900 text-neutral-content flex justify-between">
+        <Link className="btn btn-ghost normal-case text-xl ml-3" to="/">
           HR
+        </Link>
+        <Link className="btn btn-ghost normal-case text-xl mr-3" to="../Login">
+          Login
         </Link>
       </div>
     </div>
