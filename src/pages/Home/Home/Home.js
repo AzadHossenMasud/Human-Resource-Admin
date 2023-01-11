c050d1db164a2aa7fa88c21dc2276fb9a41c4639
AskatAsh <abdulaskat@gmail.com>
2023-01-11 11:26:42 +0600

    number of employee card added in home page

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -5,8 +5,20 @@ import { FiHome} from "react-icons/fi";
 import { IconName, IoPeopleOutline } from "react-icons/io5";
 const Home = () => {
   return (
-    <div >
-      Home
+    <div>
+      <h1>Home</h1>
+
+      <div className="card w-96 bg-base-100 shadow-xl mt-5">
+        <div className="card-body">
+          <h2 className="card-title justify-center">Total Employees</h2>
+          <p className="font-bold text-9xl text-center">25</p>
+          <div className="card-actions justify-center mt-3">
+            <button className="btn btn-primary bg-sky-900 hover:bg-sky-700 border-none">
+              Go to Employees
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
