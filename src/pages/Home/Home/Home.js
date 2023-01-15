4e5bb20acfdf8d7266456f9c0def5b697f99508b
ayeshaphero <ayeshatalukderctg4656@gmail.com>
2023-01-15 11:53:48 +0600

    ayesha

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, Outlet } from "react-router-dom";
-import Header from "../../Shared/Header/Header";
+import {Header} from "../../Shared/Header/Header";
 import { FiHome} from "react-icons/fi";
 import { IconName, IoPeopleOutline } from "react-icons/io5";
 import { useQuery } from "react-query";
